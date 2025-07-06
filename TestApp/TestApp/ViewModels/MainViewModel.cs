3a1d8e7130f5e8197cd8cc0f16d1d3acdaa9e78e
kusa-mochi <whoatemyapplepie@gmail.com>
2025-07-06 21:01:44 +0900

    サンプルVMのベースを作成。機能A～機能Cのウィンドウを開いているか否かをVMのプロパティで管理することを想定している。

---
+++ TestApp/TestApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.ViewModels
+{
+    internal partial class MainViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        private bool isButtonAEnabled = true;
+
+        [ObservableProperty]
+        private bool isButtonBEnabled = true;
+
+        [ObservableProperty]
+        private bool isButtonCEnabled = true;
+
+        [RelayCommand]
+        private void OpenWindowA()
+        {
+            IsButtonAEnabled = false;
+            Debug.WriteLine("open window A");
+        }
+
+        [RelayCommand]
+        private void OpenWindowB()
+        {
+            IsButtonBEnabled = false;
+            Debug.WriteLine("open window B");
+        }
+
+        [RelayCommand]
+        private void OpenWindowC()
+        {
+            IsButtonCEnabled = false;
+            Debug.WriteLine("open window C");
+        }
+    }
+}
