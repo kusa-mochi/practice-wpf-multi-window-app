d08e5e7dc93ed271e86b03fdb9a410a17a175699
kusa-mochi <whoatemyapplepie@gmail.com>
2025-07-06 21:02:57 +0900

    ダイアログを開くためのサービスクラスのひな型を作成。

---
+++ TestApp/TestApp/Services/DialogService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp.Services
+{
+    internal class DialogService : IDialogService
+    {
+        public void ShowModelessDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
